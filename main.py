c2fd631e041cdf7b75ae81fba1c72ec15116ed65
Conor Maguire <conormag94@gmail.com>
2016-03-31 14:29:30 +0100

    Update to python 3

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from Tkinter import *
+import tkinter as tk
 
-root - Tk()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-w = Label(root, text="Hello world")
-w.pack()
+    def createWidgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
 
-root.mainloop()
+        self.QUIT = tk.Button(self, text="QUIT", fg="red",
+                                            command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
